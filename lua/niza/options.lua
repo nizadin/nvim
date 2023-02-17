c27539817d15888b0aea046118eddba8474ea8a2
Niza <niza@outlook.sk>
2023-02-17 18:26:07 +0100

    Add bunch of plugins

--- lua/niza/options.lua
+++ lua/niza/options.lua
@@ -1,7 +1,31 @@
+vim.opt.guicursor = ""
+
+vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.writebackup = false
+
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
 vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
 vim.opt.number = true
 vim.opt.relativenumber = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.scrolloff = 8
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "80"
+
+vim.opt.termguicolors = true
+vim.g.mapleader = " "
--- lua/niza/remap.lua
+++ lua/niza/remap.lua
@@ -1,2 +1,20 @@
 vim.g.mapleader = " "
+
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set("x", "<leader>P", "\"_dP")
+
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+vim.keymap.set("n", "<leader>d", "\"_d")
+vim.keymap.set("v", "<leader>d", "\"_d")
+
+vim.keymap.set("n", "Q", "<nop>")
