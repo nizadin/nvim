a5ccdaaf28753381866ba0de8878cc638fcbb1ef
Niza <niza@outlook.sk>
2023-03-04 19:10:25 +0100

    Tune telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,34 +1,53 @@
+require("telescope").setup({
+  defaults = {
+    wrap_results = true,
+    dynamic_preview_title = true,
+    color_devicons = false,
+    file_ignore_patterns = {
+      "^node_modules/"
+    },
+    layout_strategy = "vertical",
+    layout_config = {
+      prompt_position = "top"
+    },
+    cache_picker = {
+      num_pickers = 3
+    }
+  }
+})
+
 local builtin = require("telescope.builtin")
 
--- vim.keymap.set("n", "<leader>fr", builtin.oldfiles, {
---   desc = "[?] [f]ind [r]ecently opened files"
--- })
-
 vim.keymap.set("n", "<leader>b", builtin.buffers, {
-  desc = "Find [b]uffers"
+  desc = "Find buffers"
 })
 
-vim.keymap.set("n", "<leader>f", builtin.git_files, {
-  desc = "Git [f]iles"
+vim.keymap.set("n", "<leader>f", builtin.find_files, {
+  desc = "Git files"
 })
 
-vim.keymap.set("n", "<leader>pf", builtin.find_files, {
-  desc = "[p]roject [f]iles"
+vim.keymap.set("n", "<leader>pf", builtin.git_files, {
+  desc = "Project files"
 })
 
 vim.keymap.set("n", "<leader>pfa",
   function() vim.cmd("Telescope find_files hidden=true") end, {
-    desc = "[p]roject [f]iles [a]ll"
+    desc = "Project files all (including hidden)"
   })
 
 vim.keymap.set("n", "<leader>F", builtin.live_grep, {
-  desc = "[F]ind grep"
+  desc = "Find grep"
 })
+
+vim.keymap.set("n", "<leader>rp", builtin.resume, {
+  desc = "Resume last search"
+})
+
 vim.keymap.set("n", "gr", builtin.lsp_references, {
-  desc = "Go to [r]eferences"
+  desc = "Go to references"
 })
 vim.keymap.set("n", "<leader>H", builtin.help_tags, {
-  desc = "Find [H]elp"
+  desc = "Find help"
 })
 
 vim.keymap.set("n", "<leader>d", function()
@@ -36,17 +55,17 @@ vim.keymap.set("n", "<leader>d", function()
     bufnr = 0
   })
 end, {
-  desc = "Find [d]iagnostics"
+  desc = "Find diagnostics"
 })
 
 vim.keymap.set("n", "<leader>D", builtin.diagnostics, {
-  desc = "Find all [D]iagnostics"
+  desc = "Find all diagnostics"
 })
 
 vim.keymap.set("n", "<leader>s", builtin.lsp_document_symbols, {
-  desc = "Document [s]ymbols"
+  desc = "Document symbols"
 })
 
 vim.keymap.set("n", "<leader>ps", builtin.lsp_workspace_symbols, {
-  desc = "[p]roject [s]ymbols"
+  desc = "Project symbols"
 })
